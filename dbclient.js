3469d480f122b4b06940dfcf390f5f6d03a5e120
Tarun Rama Krishnan Viat <35773760+Tarunnair@users.noreply.github.com>
2020-05-14 23:36:47 +0530

    update with promises

--- dbclient.js
+++ dbclient.js
@@ -1,78 +1,38 @@
 'use strict';
 
-const region = 'region';//Specify the region
-const endpoint = 'DAX ENDPOINT'; // specify the endpoint
 const AWS = require('aws-sdk');
-AWS.config.update({
-  region: region
-});
-
 const AmazonDaxClient = require('amazon-dax-client');
-let daxClient = null;
-if (endpoint) {
-  console.log('Dax Endpoint', endpoint, process.env.DAX_ENDPOINT);
-  let dax = new AmazonDaxClient({ endpoints: [endpoint], region: region });
-  daxClient = new AWS.DynamoDB.DocumentClient({ service: dax });
-}
-let docClient = new AWS.DynamoDB.DocumentClient();
-let client = daxClient ? daxClient : docClient;
 
-const query = (params) => {
-  return new Promise((resolve, reject) => {
-    client.query(params, (err, data) => {
-      if (err)
-        reject(err);
-      else 
-        resolve(data);
-    }).on('error', (error) => {
-      reject(error); //error found on event listener. this error is different from err.
-    });
-  });
+const { DAX_CLUSTER_ENDPOINT, REGION } = process.env;
+
+function createDynamoClient() {
+  if (DAX_CLUSTER_ENDPOINT) {
+    const dax = new AmazonDaxClient({ endpoints: [DAX_CLUSTER_ENDPOINT], region: REGION });
+    return new AWS.DynamoDB.DocumentClient({ region: REGION, service: dax });
+  }
+  return new AWS.DynamoDB.DocumentClient({ region: REGION });
+}
+const client = createDynamoClient();
+
+async function put(params) {
+  return client.put(params).promise();
 }
 
-const scan = (params) => {
+async function scan(params) {
   return client.scan(params).promise();
 }
 
-const dynamoQuery = (params) => {
-  return new Promise((resolve, reject) => {
-    docClient.query(params, (err, data) => {
-      if (err)
-        reject(err);
-      else 
-        resolve(data);
-    }).on('error', (error) => {
-      reject(error); //error found on event listener. this error is different from err.
-    });
-  });
+async function query(params) {
+  return client.query(params).promise();
 }
 
-const put = (params) => client.put(params).promise();
-
-const deleteObj = (params) => client.delete(params).promise();
-
-const update = (params) => client.update(params).promise();
-
-const get = (params) => {
-  return new Promise((resolve, reject) => {
-    client.get(params, (err, data) => {
-      if (err)
-        reject(err);
-      else 
-        resolve(data);
-    }).on('error', (error) => {
-      reject(error); //error found on event listener. this error is different from err.
-    });
-  });
+async function get(params) {
+  return client.get(params).promise();
 }
-
 
 module.exports = {
-  query,
-  scan,
-  dynamoQuery,
   put,
-  delete: deleteObj,
-  update,
-  get
+  get,
+  scan,
+  query
 };
